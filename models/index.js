e1043b70a43f706529ab80dcae722a97ac7b8f2e
Christopher <leechrs1@gmail.com>
2022-06-30 17:14:44 -0500

    post

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreginKey: 'user_id',
+})
+
 module.exports = { User, Post };
