670843228f731770f8965ca1123824e51e8d52c6
Christopher <leechrs1@gmail.com>
2022-06-30 22:36:27 -0500

    comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection')
+const { Post, User, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('==========');
     Post.findAll({
-        attributes: ['id', 'title', 'createdAt'],
-        order: [['created_at', 'DECS']],
+        attributes: ['id', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes:['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -29,6 +38,14 @@ router.get('/:id',(req, res) => {
         },
         attributes: ['id', 'title', 'created_at'],
         include: [
+            {
+                model: Comment,
+                attributes:['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -95,7 +112,7 @@ router.delete('/:id', (req, res) => {
     })
     .then(dbPostData => {
         if (!dbPostData) {
-            res.status(404).json({ message: 'No post founbd with this id' });
+            res.status(404).json({ message: 'No post found with this id' });
             return;
         }
         res.json(dbPostData);
